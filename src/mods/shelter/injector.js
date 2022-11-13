8df0d0cfbb5ec366bcb0d0db2c8cd40cedd3b770
Tomat <xxlennygamerxx@gmail.com>
2022-11-13 14:53:45 -0800

    Update shelter injector.

--- src/mods/shelter/injector.js
+++ src/mods/shelter/injector.js
@@ -1,126 +1,115 @@
-const shelterPreloadScript = `
-const { webFrame, ipcRenderer } = require("electron");
+const SHELTER_PRELOAD_SCRIPT = `
+const { ipcRenderer, webFrame } = require("electron");
 
-const bundle = ipcRenderer.sendSync("SHELTER_FHDIUSF");
-if (bundle) webFrame.executeJavaScript(bundle);
-const originalPreload = require("./preload.json").path;
-require(originalPreload);
+ipcRenderer.invoke("SHELTER_BUNDLE_FETCH").then((bundle) => {
+    webFrame.executeJavaScript(bundle);
+});
+
+const originalPreload = ipcRenderer.sendSync("SHELTER_ORIGINAL_PRELOAD");
+if (originalPreload) require(originalPreload);
 `;
 
 module.exports = () => {
     const electron = require("electron");
     const path = require("path");
     const fs = require("fs");
+    const https = require("https");
 
-    const basePath = path.join(path.dirname(require.main.filename), "..");
+    function logShelter(...args) {
+        global.log("ShelterInjector", ...args);
+        console.log("[shelter]", ...args);
+    }
 
-    const electronCache = require.cache[require.resolve("electron")];
+    logShelter("Loading...");
 
-    //#region CSP Removal
+    // #region Bundle
+
+    const remoteUrl = process.env.SHELTER_BUNDLE_URL || "https://raw.githubusercontent.com/uwu/shelter-builds/main/shelter.js";
+    const localBundle = process.env.SHELTER_DIST_PATH;
+
+    let shelterBundle = "";
+
+    if (localBundle) {
+        shelterBundle = fs.readFileSync(path.join(localBundle, "shelter.js"), "utf8");
+        shelterBundle += `\n//# sourceMappingURL=file:////${path.join(localBundle, "shelter.js.map")}`;
+    } else {
+        const req = https.get(remoteUrl);
+
+        req.on("response", (res) => {
+            const chunks = [];
+
+            res.on("data", (chunk) => chunks.push(chunk));
+            res.on("end", () => {
+                shelterBundle = Buffer.concat(chunks).toString("utf-8");
+
+                if (!shelterBundle.includes("//# sourceMappingURL=")) shelterBundle += `\n//# sourceMappingURL=${remoteUrl + ".map"}`;
+            });
+        });
+
+        req.end();
+    }
+
+    // #endregion
+
+    // #region IPC
+
+    electron.ipcMain.on("SHELTER_ORIGINAL_PRELOAD", (event) => {
+        event.returnValue = event.sender.originalPreload;
+    });
+
+    electron.ipcMain.handle("SHELTER_BUNDLE_FETCH", async (event) => {
+        if (!shelterBundle) await Promise((r) => setImmediate(r));
+        return shelterBundle;
+    });
+
+    // #endregion
+
+    // #region CSP
+
+    // TODO: nucleus: generalize this elsewhere
     electron.app.on("ready", () => {
-        // Removes CSP
         electron.session.defaultSession.webRequest.onHeadersReceived(({ responseHeaders }, done) => {
             const cspHeaders = Object.keys(responseHeaders).filter((name) => name.toLowerCase().startsWith("content-security-policy"));
-
-            for (const header of cspHeaders) {
-                delete responseHeaders[header];
-            }
-
+            for (const header of cspHeaders) delete responseHeaders[header];
             done({ responseHeaders });
         });
 
-        // Prevents other mods from removing CSP
-        electronCache.exports.session.defaultSession.webRequest.onHeadersReceived = () => {
-            console.log("[RawDog] Prevented CSP from being modified...");
-        };
+        electron.session.defaultSession.webRequest.onHeadersReceived = () => {};
     });
-    //#endregion
 
-    let injected = false;
+    // #endregion
 
-    const { BrowserWindow } = electron;
-    const propertyNames = Object.getOwnPropertyNames(electronCache.exports);
+    // #region ProxiedBrowserWindow
 
-    delete electronCache.exports;
-    // Make a new electron that will use the new 'BrowserWindow'
-    const newElectron = {};
-    for (const propertyName of propertyNames) {
-        Object.defineProperty(newElectron, propertyName, {
-            ...Object.getOwnPropertyDescriptor(electron, propertyName),
-            get: () =>
-                propertyName === "BrowserWindow"
-                    ? class extends BrowserWindow {
-                          constructor(opts) {
-                              if (opts.resizable && !injected) {
-                                  let originalPreload = JSON.stringify({
-                                      path: opts.webPreferences.preload
-                                  });
-                                  fs.writeFileSync(path.join(basePath, "preload.json"), originalPreload);
-                                  fs.writeFileSync(path.join(basePath, "shelterPreload.js"), shelterPreloadScript);
-                                  opts.webPreferences.preload = path.join(basePath, "shelterPreload.js");
-                                  injected = true;
-                              }
+    const ProxiedBrowserWindow = new Proxy(electron.BrowserWindow, {
+        construct(target, args) {
+            const options = args[0];
+            let originalPreload;
 
-                              const window = new BrowserWindow(opts);
-                              return window;
-                          }
-                      }
-                    : electron[propertyName]
-        });
-    }
+            if (options.webPreferences?.preload && options.title) {
+                // We replace the preload instead of using setPreloads because of some differences in internal behavior.
+                const basePath = path.join(path.dirname(require.main.filename), "..");
+                fs.writeFileSync(path.join(basePath, "shelter-preload.js"), SHELTER_PRELOAD_SCRIPT);
 
-    electronCache.exports = newElectron;
-
-    (async () => {
-        let shelterBundle = "";
-        const remoteUrl = process.env.SHELTER_BUNDLE_URL || "https://raw.githubusercontent.com/uwu/shelter-builds/main/shelter.js";
-        const localBundle = process.env.SHELTER_DIST_PATH;
-
-        try {
-            if (localBundle) {
-                shelterBundle = readFileSync(resolve(join(localBundle, "shelter.js")), "utf8");
-                shelterBundle += `\n//# sourceMappingURL=file:////${resolve(join(localBundle, "shelter.js.map"))}`;
-            } else {
-                // shelterBundle = await (await fetch(remoteUrl)).text();
-
-                let done = false;
-                const http = require("https"); // fucking hell
-                http.get(remoteUrl, (res) => {
-                    res.on("data", (d) => {
-                        shelterBundle += d;
-                    });
-                    res.on("end", () => (done = true));
-                });
-
-                while (!done)
-                    // don't say i didn't warn you
-                    await new Promise((r) => setTimeout(r));
-
-                if (!shelterBundle.includes("//# sourceMappingURL=")) {
-                    shelterBundle += `\n//# sourceMappingURL=${remoteUrl + ".map"}`;
-                }
+                originalPreload = options.webPreferences.preload;
+                options.webPreferences.preload = path.join(basePath, "shelter-preload.js");
             }
-        } catch (err) {
-            console.error("[shelter-inject] Failed to inject.\n", err);
-            const options = {
-                type: "error",
-                buttons: ["Continue", "Close Discord"],
-                defaultId: 0,
-                cancelId: 1,
-                message: "Shelter failed to load from local dist. \nCheck console for more info.",
-                detail: err.message
-            };
-            let pressedButtonId = electron.dialog.showMessageBoxSync(null, options);
-            if (pressedButtonId == 1) {
-                process.exit();
-            }
+
+            const window = new target(options);
+            window.webContents.originalPreload = originalPreload;
+            return window;
         }
+    });
 
-        electron.ipcMain.on("SHELTER_FHDIUSF", (event) => {
-            event.returnValue = shelterBundle;
-        });
+    const electronPath = require.resolve("electron");
+    delete require.cache[electronPath].exports;
+    require.cache[electronPath].exports = {
+        ...electron,
+        BrowserWindow: ProxiedBrowserWindow
+    };
 
-        log("ShelterInjector", "Shelter early load hopefully successful.");
-        console.log("[shelter-inject] early load hopefully successful");
-    })();
+    // #endregion
+
+    // TODO: nucleus: Do we want to bother logging this? Trying to keep parity with the shelter injector...
+    logShelter("Starting original...");
 };
